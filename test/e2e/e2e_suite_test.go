52ad96cb8c713ffa11a4d31612f8a02139487f64
Talor Itzhak <titzhak@redhat.com>
2023-03-14 19:56:42 +0200

    e2e: setup test suite and utils
    Setup the infrastructure and tools that are needed for wrting
    e2e tests
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

---
+++ test/e2e/e2e_suite_test.go
@@ -0,0 +1,80 @@
+package e2e_test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+)
+
+type TestFixture struct {
+	Cli    client.Client
+	K8SCli *kubernetes.Clientset
+	NS     *corev1.Namespace
+}
+
+var fixture TestFixture
+
+func TestE2e(t *testing.T) {
+	BeforeSuite(func() {
+		Expect(initClient()).ToNot(HaveOccurred())
+		Expect(initK8SClient()).ToNot(HaveOccurred())
+		Expect(createNamespace("mixedcpus-testing-")).ToNot(HaveOccurred())
+	})
+	AfterSuite(func() {
+		Expect(fixture.Cli.Delete(context.TODO(), fixture.NS)).ToNot(HaveOccurred())
+	})
+
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "E2e Suite")
+
+}
+
+func initClient() error {
+	cfg, err := config.GetConfig()
+	if err != nil {
+		return err
+	}
+
+	fixture.Cli, err = client.New(cfg, client.Options{})
+	return err
+}
+
+func initK8SClient() error {
+	cfg, err := config.GetConfig()
+	if err != nil {
+		return err
+	}
+	fixture.K8SCli, err = kubernetes.NewForConfig(cfg)
+	return err
+}
+
+func createNamespace(prefix string) error {
+	ns := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: prefix,
+		},
+	}
+	if err := fixture.Cli.Create(context.TODO(), ns); err != nil {
+		return err
+	}
+	fixture.NS = ns
+	return nil
+}
+
+// TODO make it possible to read directly from DaemonSet
+func GetSharedCPUs() string {
+	cpus, ok := os.LookupEnv("E2E_SHARED_CPUS")
+	if !ok {
+		return ""
+	}
+	return cpus
+}
--- test/e2e/pods/pods.go
+++ test/e2e/pods/pods.go
@@ -1,1 +1,133 @@
 package pods
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/remotecommand"
+	"k8s.io/kubernetes/pkg/kubelet/cm/cpuset"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+)
+
+const (
+	PauseImage   = "quay.io/openshift-kni/pause:test-ci"
+	PauseCommand = "/pause"
+)
+
+type Options func(*corev1.Pod)
+
+func Make(name, namespace string, opts ...Options) *corev1.Pod {
+	var zero int64
+	p := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: corev1.PodSpec{
+			TerminationGracePeriodSeconds: &zero,
+			Containers: []corev1.Container{
+				{
+					Name:    name + "-cnt",
+					Image:   PauseImage,
+					Command: []string{PauseCommand},
+				},
+			},
+		},
+	}
+	for _, opt := range opts {
+		opt(p)
+	}
+	return p
+}
+
+func WithRequests(list corev1.ResourceList, ids ...int) func(pod *corev1.Pod) {
+	if ids == nil {
+		ids = append(ids, 0)
+	}
+	return func(pod *corev1.Pod) {
+		for _, id := range ids {
+			pod.Spec.Containers[id].Resources.Requests = list
+		}
+	}
+}
+
+func WithLimits(list corev1.ResourceList, ids ...int) func(pod *corev1.Pod) {
+	if ids == nil {
+		ids = append(ids, 0)
+	}
+	return func(pod *corev1.Pod) {
+		for _, id := range ids {
+			pod.Spec.Containers[id].Resources.Limits = list
+		}
+	}
+}
+
+// GetAllowedCPUs returns a CPUSet of cpus that the pod's containers
+// are allowed to access
+func GetAllowedCPUs(c *kubernetes.Clientset, pod *corev1.Pod) (*cpuset.CPUSet, error) {
+	// TODO is this reliable enough or we should check cgroups directly?
+	cmd := []string{"/bin/sh", "-c", "grep Cpus_allowed_list /proc/self/status | cut -f2"}
+	out, err := Exec(c, pod, cmd)
+	if err != nil {
+		return nil, fmt.Errorf("failed to run command: %s out: %s; %w", cmd, out, err)
+	}
+
+	cpus, err := cpuset.Parse(strings.Trim(string(out), "\r\n"))
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse cpuset when input is: %s; %w", out, err)
+	}
+	return &cpus, nil
+}
+
+func Exec(c *kubernetes.Clientset, pod *corev1.Pod, command []string) ([]byte, error) {
+	var outputBuf bytes.Buffer
+	var errorBuf bytes.Buffer
+
+	req := c.CoreV1().RESTClient().
+		Post().
+		Namespace(pod.Namespace).
+		Resource("pods").
+		Name(pod.Name).
+		SubResource("exec").
+		VersionedParams(&corev1.PodExecOptions{
+			Container: pod.Spec.Containers[0].Name,
+			Command:   command,
+			Stdin:     true,
+			Stdout:    true,
+			Stderr:    true,
+			TTY:       true,
+		}, scheme.ParameterCodec)
+
+	cfg, err := config.GetConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	exec, err := remotecommand.NewSPDYExecutor(cfg, "POST", req.URL())
+	if err != nil {
+		return nil, err
+	}
+
+	err = exec.StreamWithContext(context.TODO(), remotecommand.StreamOptions{
+		Stdin:  os.Stdin,
+		Stdout: &outputBuf,
+		Stderr: &errorBuf,
+		Tty:    true,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to run command %v: output %q; error %q; %w", command, outputBuf.String(), errorBuf.String(), err)
+	}
+
+	if errorBuf.Len() != 0 {
+		return nil, fmt.Errorf("failed to run command %v: output %q; error %q", command, outputBuf.String(), errorBuf.String())
+	}
+
+	return outputBuf.Bytes(), nil
+}
