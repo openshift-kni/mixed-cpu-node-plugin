5a5fe7a874e6c368d2ac11519c1e1ab96ec8919e
Talor Itzhak <titzhak@redhat.com>
2023-03-16 13:30:45 +0200

    ci: e2e: skip if not enough cpus
    Check node's resources and skip the test if it's insufficient
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

--- test/e2e/mixedcpus_test.go
+++ test/e2e/mixedcpus_test.go
@@ -19,9 +19,28 @@ import (
 	"github.com/Tal-or/nri-mixed-cpu-pools-plugin/test/e2e/pods"
 )
 
+const (
+	minimalCPUsForTesting  = 2
+	minimalNodesForTesting = 1
+)
+
 var _ = Describe("Mixedcpus", func() {
 	When("a pod gets request for shared cpu device", func() {
 		It("should contains the shared cpus under its cgroups", func() {
+			nodeList := &corev1.NodeList{}
+			Expect(fixture.Cli.List(context.TODO(), nodeList)).ToNot(HaveOccurred())
+
+			var nodes []*corev1.Node
+			for i := 0; i < len(nodeList.Items); i++ {
+				node := &nodeList.Items[i]
+				if node.Status.Allocatable.Cpu().Value() >= minimalCPUsForTesting {
+					nodes = append(nodes, node)
+				}
+			}
+			if len(nodes) < minimalNodesForTesting {
+				Skip(fmt.Sprintf("minimum of %d nodes with minimum of %d cpus are needed", minimalNodesForTesting, minimalCPUsForTesting))
+			}
+
 			By("creating a pod with shared-cpu device")
 			pod := pods.Make("test", fixture.NS.Name, pods.WithLimits(corev1.ResourceList{
 				corev1.ResourceCPU:               resource.MustParse("1"),
