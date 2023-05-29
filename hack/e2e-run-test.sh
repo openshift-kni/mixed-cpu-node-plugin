10cc3e38dfe8fd65e424c25a6ec96ef8a997981b
Talor Itzhak <titzhak@redhat.com>
2023-05-29 14:03:24 +0300

    e2e: print e2e test args
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

--- hack/e2e-run-test.sh
+++ hack/e2e-run-test.sh
@@ -4,6 +4,6 @@ E2E_SHARED_CPUS=${1}
 E2E_SETUP=${2}
 E2E_TEARDOWN=${3}
 
-echo "Running e2e test"
+echo "Running e2e test with args: E2E_SHARED_CPUS=${E2E_SHARED_CPUS}, E2E_SETUP=${E2E_SETUP}, E2E_TEARDOWN=${E2E_TEARDOWN}"
 E2E_SETUP=${E2E_SETUP} E2E_SHARED_CPUS=${E2E_SHARED_CPUS} E2E_TEARDOWN=${E2E_TEARDOWN} \
 build/bin/e2e_test --ginkgo.v
